c9a1c1a1b7152df6a123d479697c00e296f4cae5
ZVNexus <zvnexus@outlook.com>
2019-07-18 18:51:04 -0400

    Redo Titan M stuff

--- recovery/recovery_crosshatch_ui.cpp
+++ recovery/recovery_crosshatch_ui.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,35 +15,29 @@
  */
 
 #include <android-base/endian.h>
+#include <android-base/logging.h>
 
 #include <app_nugget.h>
 #include <nos/debug.h>
 #include <nos/NuggetClient.h>
 
-#include "partitions.hpp"
-#include "twcommon.h"
-#include "gui/gui.h"
-#include <cutils/properties.h>
-#include "unistd.h"
+#include <device.h>
+#include <screen_ui.h>
 
 namespace android {
 namespace device {
 namespace google {
-namespace bonito {
+namespace crosshatch {
 
 namespace {
 
 /** Wipe user data from Titan M. */
 bool WipeTitanM() {
-    LOGINFO("Stopping Weaver services\n");
-    property_set("weaver.stop", "1");
-    sleep(1);
-
     // Connect to Titan M
     ::nos::NuggetClient client;
     client.Open();
     if (!client.IsOpen()) {
-        LOGERR("Failed to connect to Titan M\n");
+        LOG(ERROR) << "Failed to connect to Titan M";
         return false;
     }
 
@@ -54,27 +48,29 @@ bool WipeTitanM() {
     const uint32_t status
             = client.CallApp(APP_ID_NUGGET, NUGGET_PARAM_NUKE_FROM_ORBIT, magic, nullptr);
     if (status != APP_SUCCESS) {
-		LOGERR("Titan M user data wipe failed: %s (%i)\n", ::nos::StatusCodeString(status).c_str(), status);
+        LOG(ERROR) << "Titan M user data wipe failed: " << ::nos::StatusCodeString(status)
+                   << " (" << status << ")";
         return false;
     }
 
-    LOGINFO("Titan M wipe successful\n");
+    LOG(INFO) << "Titan M wipe successful";
     return true;
 }
 
 } // namespace
 
-class BonitoDevice : public ::BasePartition
+class CrosshatchDevice : public ::Device
 {
 public:
-    BonitoDevice() {}
+    CrosshatchDevice(::ScreenRecoveryUI* const ui) : ::Device(ui) {}
 
     /** Hook to wipe user data not stored in /data */
-    bool PostWipeEncryption() override {
+    bool PostWipeData() override {
         // Try to do everything but report a failure if anything wasn't successful
         bool totalSuccess = true;
+        ::RecoveryUI* const ui = GetUI();
 
-        gui_print("Wiping Titan M...\n");
+        ui->Print("Wiping Titan M...\n");
         if (!WipeTitanM()) {
             totalSuccess = false;
         }
@@ -85,12 +81,12 @@ public:
     }
 };
 
-} // namespace bonito
+} // namespace crosshatch
 } // namespace google
 } // namespace device
 } // namespace android
 
-BasePartition *make_partition()
+Device *make_device()
 {
-    return new ::android::device::google::bonito::BonitoDevice();
+    return new ::android::device::google::crosshatch::CrosshatchDevice(new ::ScreenRecoveryUI);
 }
